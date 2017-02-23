fc57fee94c74110895e0d22d2258d470a57ad1ba
Jan Rybizki <jan.rybizki@weyhausen.de>
2017-02-23 15:19:16 +0100

    conf

--- doc/conf.py
+++ doc/conf.py
@@ -115,18 +115,18 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #html_theme_options = {}
-html_theme_options = {
-        'github_button': True,
-        'github_user': 'jan-rybizki',
-        'github_repo': 'Chempy',
-        'github_banner': True,
-}
+#html_theme_options = {
+#        'github_button': True,
+#        'github_user': 'jan-rybizki',
+#        'github_repo': 'Chempy',
+#        'github_banner': True,
+#}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []

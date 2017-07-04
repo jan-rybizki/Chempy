cd630e3982aaf331b2aa5b6d7390310be5104a79
Jan Rybizki <jan.rybizki@weyhausen.de>
2017-07-04 10:11:43 +0200

    high alpha sequence

--- Chempy/parameter.py
+++ Chempy/parameter.py
@@ -15,7 +15,8 @@ class ModelParameters(object):
 	# Observational constraints
 	#stellar_identifier_list = ['Proto-sun', 'Arcturus', 'B-stars']
 	#stellar_identifier_list = ['2M01233744+3414451', '2M02484368+3106550', '2M05510326+1129561', '2M09031459+0648573', '2M09422500+4846338', '2M02011031+2426397', '2M09055837+0505324', '2M20092234+5601366']
-	indices = [78,130,122,156,113,34, 128,167] # low alpha sequence
+	#indices = [78,130,122,156,113,34, 128,167] # low alpha sequence
+	indices = [85, 94, 15, 110, 30, 11, 7, 3] # high alpha sequence
 	stellar_identifier_list = []
 	for item in indices:
 		stellar_identifier_list.append("Rob_%d" %item)

479a14a7d5a1e9dd593f348008b770dd5fad277d
jan-rybizki <jan.rybizki@weyhausen.de>
2018-07-13 14:23:31 +0200

    v0.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def readme():
         return f.read()
 
 setup(name = "Chempy",
-    version = 0.1,
+    version = 0.2,
     description = "chemical evolution modeling",
     long_description = readme(),
     author = "Jan Rybizki",
